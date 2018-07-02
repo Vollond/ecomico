ef329af7f41dce63f47ed545fbd17db13700352b
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-02 18:15:26 +0300

    1

--- bot.py
+++ bot.py
@@ -2,8 +2,11 @@ import config
 import telebot
 import os
 import psycopg2
+from flask import Flask, request
 import logging
-print("123")
+
+bot = telebot.TeleBot(config.token)
+
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -12,21 +15,24 @@ cursor = conn.cursor()
 # Выполняем запрос.
 cursor.execute("SELECT * FROM users")
 row = cursor.fetchone()
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+server = Flask(__name__)
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://ecomico.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+    return "?", 200
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
  
  
-TOKEN = config.token
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://ecomico.herokuapp.com/" + TOKEN)
-updater.idle() 
  
- 
- 
-bot = telebot.TeleBot(config.token)
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
