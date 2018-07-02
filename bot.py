8f0b66c5df961d8a31965553fda7461e587f0b17
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-02 16:11:54 +0300

    1

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import config
 import telebot
 import os
 import psycopg2
-import Flask, request
+from flask import Flask, request
 import logging
 print("123")
 DATABASE_URL = os.environ['DATABASE_URL']
